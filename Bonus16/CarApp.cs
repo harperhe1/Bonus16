4a64a9b8654e482a459a1fbae4f6949c7f4c1f7d
Heather <harperhe@hotmail.com>
2018-10-25 11:13:19 -0400

    initial commit

---
+++ Bonus16/CarApp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus16
+{
+    class CarApp
+    {
+        public static void BuildCarList()
+        {
+            List<Car> myCarList = new List<Car>();
+            int userInput = GetNumberOfCars();
+          
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                Console.WriteLine("Enter car # "+ i +" make: ");
+                string make = Console.ReadLine();
+                Console.WriteLine("Enter car # " + i + " model: ");
+                string model = Console.ReadLine();
+                Console.WriteLine("Enter car # " + i + " year:");
+                int year = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter car # " + i + " price:");
+                double price = double.Parse(Console.ReadLine());
+
+                Car myCar = new Car();
+                myCar.Make = make;
+                myCar.Model = model;
+                myCar.Year = year;
+                myCar.Price = price;
+
+                myCarList.Add(myCar);
+
+            }
+
+            foreach (Car item in myCarList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            
+        }
+
+        public static int GetNumberOfCars()
+        {
+            Console.WriteLine("Please enter number of cars you want:");
+            int userInput = int.Parse(Console.ReadLine());
+            return userInput;
+        }
+
+    }
+}
---
+++ Bonus16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus16
+{   //number of cars
+    //details for each car
+    //print all cars
+
+        //1. new class Car
+        //2. string for make,model, int year, double for price
+        //field is private
+        //property is public
+        //3. default constructor
+        //4. another constructor that takes input for each of the 4 arguments
+        //5. override ToString method returning a formatted string with the car details
+        //6. class CarApp - creates a car object, and displays the car information
+  
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarApp.BuildCarList();
+
+        }
+    }
+}
